02e3417895662f2e4ecf25e8d4e4aec841f0af4a
magno <magnoboc@gmail.com>
2024-01-18 10:33:06 -0300

    added cors

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM node:20
 
 WORKDIR /app
 
-COPY ./package.json .
-RUN npm ci
+COPY package*.json ./
+RUN npm install
 COPY . .
 
 EXPOSE 3000
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const booksApi = require('./routes/books');
-const errorHandler = require('./middlewares/errorHandler');
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const booksApi = require("./routes/books");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
 
 // App
 const app = express();
-app.use(bodyParser.json())
-app.use("/books", booksApi)
-app.use(errorHandler)
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/books", booksApi);
+app.use(errorHandler);
 
 app.listen(process.env.PORT);
 console.log(`Running on http://0.0.0.0:${process.env.PORT}`);
